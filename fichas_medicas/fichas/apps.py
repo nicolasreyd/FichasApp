c1dbb1388e678bbb8a974406b3eacf90c997057d
unknown <nicolasreydominguez@gmail.com>
2020-06-21 12:30:02 -0300

    Restart

--- fichas_medicas/fichas/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FichasConfig(AppConfig):
-    name = 'fichas'
--- fichas_medicas/fichas/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path,include
-from . import views
-
-urlpatterns = [
-    path('', views.fichas_form),
-    path('list/',views.fichas_list)
-]
